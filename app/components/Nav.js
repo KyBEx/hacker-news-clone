165255af2e2de0692dfc1a917f2b5da53adb7928
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2020-07-20 19:16:04 -0600

    added styling to Top and New links

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "rgb(252, 3, 3)",
+};
 
 export default function Nav() {
   return (
-    <div>
-      <ul>
-        <li>Top</li>
-        <li>New</li>
+    <div className="row space-between">
+      <ul className="row">
+        <li>
+          <NavLink exact className="nav-link" to="/" activeStyle={activeStyle}>
+            Top
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            exact
+            className="nav-link"
+            to="/new"
+            activeStyle={activeStyle}
+          >
+            New
+          </NavLink>
+        </li>
       </ul>
       <button>Darkmode</button>
     </div>
--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Nav, Top, New } from "./components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
 
 class App extends React.Component {
   render() {
