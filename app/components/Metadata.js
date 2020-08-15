9b876840bfa54111e3702af424e47dda5e22bfea
KyBEx <32848013+KyBEx@users.noreply.github.com>
2020-08-15 13:30:43 -0600

    Show posts (#4)
    * added data to metadata widget on Top page
    
    * added meta for top posts; adding styling to meta
    
    * added posts to New; added check for posts without url

--- app/components/Metadata.js
+++ app/components/Metadata.js
@@ -1,24 +1,59 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
+import Nav from "./Nav";
 
-export default function Metadata({ user, date, post = {} }) {
+export default function Metadata({
+  by,
+  time,
+  descendants,
+  id,
+  type,
+  title,
+  url,
+}) {
   return (
-    <div>
-      by {<NavLink to={`/user?id=${user.id}`}>{user.username}</NavLink>} on{" "}
-      {date}{" "}
-      {Object.keys(post).length > 0 && (
-        <span>
-          <NavLink to={`/post?id=${post.id}`}>{post.comments.length}</NavLink>{" "}
-          comments
-        </span>
+    <div className="med-padding">
+      {title && (
+        <div className="sml-padding">
+          {url ? (
+            <a className="bold-red" href={url} target="_blank">
+              {title}
+            </a>
+          ) : (
+            <NavLink className="bold-red" to={`/post?id=${id}`}>
+              {title}
+            </NavLink>
+          )}
+        </div>
       )}
+      <div>
+        by{" "}
+        {
+          <NavLink className="meta-link" to={`/user?id=${by}`}>
+            {by}
+          </NavLink>
+        }{" "}
+        on {""}
+        {time}{" "}
+        {type === "story" && (
+          <span>
+            <NavLink className="meta-link" to={`/post?id=${id}`}>
+              {descendants}
+            </NavLink>{" "}
+            comments
+          </span>
+        )}
+      </div>
     </div>
   );
 }
 
 Metadata.proptypes = {
-  user: PropTypes.object.isRequired,
-  data: PropTypes.string.isRequired,
-  post: PropTypes.object,
+  by: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string,
+  url: PropTypes.string,
+  descendants: PropTypes.number,
 };
