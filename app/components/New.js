06ba2dd21b741aaa7bb4e9ca9a3a56095c1d3b6f
KyBEx <32848013+KyBEx@users.noreply.github.com>
2020-08-14 20:05:43 -0600

    Api (#3)
    * added fetch stories functionality and error handling for Top and New components
    
    * added base requests for fetching posts, comments, users

--- app/components/New.js
+++ app/components/New.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { getNewOrTop } from "../utils/api";
 
-export default function New() {
-  return <h1>New Component</h1>;
+export default class New extends React.Component {
+  componentDidMount() {
+    getNewOrTop("newstories")
+      .then((data) => console.log("data in componentDidMount", data))
+      .catch((err) => console.log("err in component did mount", err));
+  }
+
+  render() {
+    return <h1>New Component</h1>;
+  }
 }
---
+++ app/utils/api.js
@@ -0,0 +1,71 @@
+const url = "https://hacker-news.firebaseio.com/v0";
+
+export function getNewOrTop(type) {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}/${type}.json`)
+      .then((data) => {
+        if (!data.ok) {
+          reject(
+            `Error fetching stories. Reason: ${data.status} ${data.statusText}`
+          );
+        }
+        return data.json();
+      })
+      .then((data) => data.slice(0, 50))
+      .then((data) => Promise.all(data.map(getItemById)))
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+  });
+}
+
+function getItemById(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}/item/${id}.json`)
+      .then((data) => {
+        if (!data.ok) {
+          reject(
+            `Error fetching story ${id}. Reason: ${data.status} ${data.statusText}`
+          );
+        }
+        return data.json();
+      })
+      .then((data) => resolve(data));
+  });
+}
+
+export function getUser(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}/user/${id}`)
+      .then((data) => {
+        if (!data.ok) {
+          reject(
+            `Error fetching user ${id}.  Reason: ${data.status} ${data.statusText}`
+          );
+        }
+        return data.json();
+      })
+      .then((data) => resolve(data));
+  });
+}
+
+export function getUserPosts(ids) {
+  return Promise.all(ids.map(getItemById));
+}
+
+export function getComments(ids) {
+  return Promise.all(ids.map(getItemById)).then((comments) =>
+    removeDeleted(onlyComments(removeDead(comments)))
+  );
+}
+
+function removeDead(posts) {
+  return posts.filter(Boolean).filter(({ dead }) => dead !== true);
+}
+
+function removeDeleted(posts) {
+  return posts.filter(({ deleted }) => deleted !== true);
+}
+
+function onlyComments(posts) {
+  return posts.filter(({ type }) => type === "comment");
+}
