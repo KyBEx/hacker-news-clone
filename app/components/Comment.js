fb54e2ad145fabde7d6fe2b73d3f22df9b0c2d61
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2020-08-16 16:00:19 -0600

    added Theme context to comment

--- app/components/Comment.js
+++ app/components/Comment.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Metadata from "./Metadata";
+import { ThemeConsumer } from "../context/Theme";
 
 export default function Comment(props) {
   console.log("props.text", typeof props.text);
   return (
-    <div className="comment">
-      <Metadata {...props} />
-      <p dangerouslySetInnerHTML={{ __html: props.text }}></p>
-    </div>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div
+          className={`comment ${
+            theme === "light" ? "comment-color-light" : "comment-color-dark"
+          }`}
+        >
+          <Metadata {...props} />
+          <p dangerouslySetInnerHTML={{ __html: props.text }}></p>
+        </div>
+      )}
+    </ThemeConsumer>
   );
 }
