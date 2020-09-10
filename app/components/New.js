7d9712ce87f011c83e37cedecd582fa741ac6c73
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2020-09-10 08:44:56 -0600

    added loading widget; implemented loading widget in New, Post, Top, and User components

--- app/components/New.js
+++ app/components/New.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { getNewOrTop } from "../utils/api";
 import Metadata from "./Metadata";
+import Loading from "./Loading";
 
 export default class New extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
+      loading: true,
     };
   }
   componentDidMount() {
     getNewOrTop("newstories")
-      .then((data) => this.setState({ posts: data }))
+      .then((data) => this.setState({ posts: data, loading: false }))
       .catch((err) => console.error("err in component did mount", err));
   }
 
   render() {
     return (
       <>
-        {this.state.posts.map((post) => (
-          <Metadata key={post.id} {...post} />
-        ))}
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <>
+            {this.state.posts.map((post) => (
+              <Metadata key={post.id} {...post} />
+            ))}
+          </>
+        )}
       </>
     );
   }
